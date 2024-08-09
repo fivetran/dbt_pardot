4b556518e0a0a75749784fdc9ea3b4f7c31e8540
Tyler Wood <tylercwood@gmail.com>
2024-08-08 21:27:06 -0400

    join list email dimension enhancements to pardot activities

--- models/intermediate/int_pardot__activities_join_enriched.sql
+++ models/intermediate/int_pardot__activities_join_enriched.sql
@@ -15,9 +15,19 @@ list_emails as (
 
 select 
     visitor_activity.*,
+    
+    is_mmus_formated_list_email_name,
+    list_email_natural_key,
+    list_email_sent_at,
+    list_email_keyword_segment,
+    list_email_keyword_status,
+    list_email_keyword_type,
+    list_email_name_parsed_topic,
+
     list_email_name,
     list_email_subject,
-    list_email_sent_at,
+
+    
     campaigns.campaign_name as activity_campaign_name 
 
 from visitor_activity
